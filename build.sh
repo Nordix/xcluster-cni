f4a1a57b458f620f77849174e84175829fa4748b
Lars Ekman <lars.g.ekman@est.tech>
2019-12-03 10:52:17 +0100

    Use "latest" as default tag

--- build.sh
+++ build.sh
@@ -35,12 +35,12 @@ dbg() {
 ##
 cmd_env() {
 	test -n "$__image" || __image=registry.nordix.org/cloud-native/xcluster-cni
-	test -n "$__version" || __version=master
+	test -n "$__version" || __version=latest
 	test -n "$__plugin_tar" || __plugin_tar=$HOME/Downloads/cni-plugins-linux-amd64-v0.8.2.tgz
 	test "$cmd" = "env" && set | grep -E '^(__.*)='
 }
 
-##  image [--image=name] [--version=master] [--plugin-tar=file]
+##  image [--image=name] [--version=latest] [--plugin-tar=file]
 ##    Build the "xcluster-cni" image.
 ##
 cmd_image() {
